7cd7761dcfa5ba7a1cd249b21c8883984412449e
fpemba <fatuma.pemba@yahoo.com>
2020-10-05 17:50:25 -0400

    Classes Created teacher class Updated student class with getters

---
+++ src/org/wecancodeit/Teacher.java
@@ -0,0 +1,30 @@
+package org.wecancodeit;
+
+public class Teacher {
+
+    private int id;
+    private String name;
+    private double salary;
+
+    public Teacher(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
