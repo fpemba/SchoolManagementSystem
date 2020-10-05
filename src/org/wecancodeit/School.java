773179973cb48e668d5d0762fd723d80d95c41ce
fpemba <fatuma.pemba@yahoo.com>
2020-10-05 18:51:19 -0400

    Classes Created school class

---
+++ src/org/wecancodeit/School.java
@@ -0,0 +1,51 @@
+package org.wecancodeit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+
+    private List<Teacher> teachers;
+    private List<Student> students;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudents(Student student) {
+        students.add(student);
+    }
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public void updateTotalMoneyEarned(int moneyEarned) {
+        totalMoneyEarned+=moneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    public void updateTotalMoneySpent(int moneySpent) {
+        totalMoneySpent -= moneySpent;
+    }
+}
